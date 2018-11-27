50947b6ed47dec59e532c0be47651013e02bc860
Elliott Evers <everselliott@gmail.com>
2018-11-27 12:56:32 -0700

    automated download and wav conversion from spotify playlist

---
+++ download_spotify.py
@@ -0,0 +1,5 @@
+# TODO: How to download one song at a time?  Or download diff of playlist?
+
+# CLI command
+
+# spotify_dl -l https://open.spotify.com/user/remingtonrocks/playlist/12Qq67y8qOQSotXWxJxQoH?si=EvAFtJzITFOlwveH2SgUow -o output/
