f809e479d6a9fced88e44c7dc515345895a7b388
Elliott Evers <everselliott@gmail.com>
2018-11-27 13:19:47 -0700

    sketch of initial stages of pipeline

---
+++ main.py
@@ -0,0 +1,34 @@
+# DATA SOURCE
+
+# download from Spotify/YouTube - download_spotify.py
+
+# PREPROCESSING/INTERACTIVE INFORMATION EXTRACTION
+
+# convert to WAV - conversion/mp3_to_wav.py
+
+# melody extraction - # TODO: melody_extraction.py
+
+# human assisted smoothing
+
+# chord detection # TODO: chord_detection.py
+
+# TODO: think of any useful human assistance
+
+# symbolic key detection
+
+# human assisted tonic detection
+
+# synthetic segmentation
+
+# human assisted boundary detection
+
+# EVALUATION
+
+# render MusicXML (chord symbols + full voicings)
+
+# MuseScore playback
+
+# HUMAN DISCRIMINATION
+
+# HUMAN GENERATION
+
