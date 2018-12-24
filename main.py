969cddf8ebd560f86d5fddb3d84f2e32e07f6d05
Elliott Evers <everselliott@gmail.com>
2018-12-23 17:13:00 -0700

    some test cases - yet to set up testing properly

--- main.py
+++ main.py
@@ -3,8 +3,218 @@ import convert.midi as midi_convert
 import filter.series as series_filter
 import mido
 import pandas as pd
+import itertools
+import sys
+
+from subprocess import call as call_shell
 
 filename_input = '/Users/elliottevers/Downloads/ella_dream_vocals_2.mid'
+
+####
+
+file = mido.MidiFile(ticks_per_beat=2)
+
+
+track1 = mido.MidiTrack()
+
+file.tracks.append(track1)
+
+track1.append(
+    mido.MetaMessage(
+        'set_tempo',
+        tempo=500000*2*2
+    )
+)
+
+
+track2 = mido.MidiTrack()
+
+file.tracks.append(track2)
+
+track2.append(
+    mido.MetaMessage(
+        'set_tempo',
+        tempo=500000*2*2
+    )
+)
+
+
+track3 = mido.MidiTrack()
+
+file.tracks.append(track3)
+
+track3.append(
+    mido.MetaMessage(
+        'set_tempo',
+        tempo=500000*2*2
+    )
+)
+
+
+track4 = mido.MidiTrack()
+
+file.tracks.append(track4)
+
+track4.append(
+    mido.MetaMessage(
+        'set_tempo',
+        tempo=500000*2*2
+    )
+)
+
+track1.append(
+    mido.Message(
+        'note_on',
+        note=60,
+        velocity=90,
+        time=0
+    )
+)
+
+track1.append(
+    mido.Message(
+        'note_off',
+        note=60,
+        velocity=0,
+        time=1
+    )
+)
+
+track1.append(
+    mido.Message(
+        'note_on',
+        note=67,
+        velocity=90,
+        time=1
+    )
+)
+
+track1.append(
+    mido.Message(
+        'note_off',
+        note=67,
+        velocity=0,
+        time=1
+    )
+)
+
+
+track2.append(
+    mido.Message(
+        'note_on',
+        note=60,
+        velocity=90,
+        time=0
+    )
+)
+
+track2.append(
+    mido.Message(
+        'note_off',
+        note=60,
+        velocity=0,
+        time=1
+    )
+)
+
+track2.append(
+    mido.Message(
+        'note_on',
+        note=67,
+        velocity=90,
+        time=0
+    )
+)
+
+track2.append(
+    mido.Message(
+        'note_off',
+        note=67,
+        velocity=0,
+        time=2
+    )
+)
+
+track3.append(
+    mido.Message(
+        'note_on',
+        note=60,
+        velocity=90,
+        time=0
+    )
+)
+
+track3.append(
+    mido.Message(
+        'note_on',
+        note=67,
+        velocity=90,
+        time=1
+    )
+)
+
+track3.append(
+    mido.Message(
+        'note_off',
+        note=60,
+        velocity=0,
+        time=1
+    )
+)
+
+track3.append(
+    mido.Message(
+        'note_off',
+        note=67,
+        velocity=0,
+        time=1
+    )
+)
+
+track4.append(
+    mido.Message(
+        'note_on',
+        note=60,
+        velocity=90,
+        time=0
+    )
+)
+
+track4.append(
+    mido.Message(
+        'note_on',
+        note=67,
+        velocity=90,
+        time=1
+    )
+)
+
+track4.append(
+    mido.Message(
+        'note_off',
+        note=67,
+        velocity=0,
+        time=1
+    )
+)
+
+track4.append(
+    mido.Message(
+        'note_off',
+        note=60,
+        velocity=0,
+        time=1
+    )
+)
+
+filename_out = '/Users/elliottevers/Downloads/simulated_data.mid'
+
+file.save(filename_out)
+
+call_shell(["open", "-a", "/Applications/MidiYodi 2018.1.app/", filename_out])
+
+exit(0)
+####
 
 bpm_file = 75
 
