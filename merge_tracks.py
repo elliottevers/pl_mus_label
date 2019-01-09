e5287f6a97cea13dcc7471490ff840967a5a8c1c
Elliott Evers <everselliott@gmail.com>
2019-01-09 13:53:30 -0700

    generalizing an often used program

--- merge_tracks.py
+++ merge_tracks.py
@@ -1,42 +1,14 @@
 from mido import MidiFile, Message, MidiTrack
 from subprocess import call as call_shell
 
+dir_tracks = '/Users/elliottevers/Downloads/'
 
-# dir_tracks = '/Users/elliottevers/Documents/tracks/export/ableton/'
-dir_tracks = '/Users/elliottevers/Documents/tracks/export/ableton/biab_sync/'
-filename_bass = dir_tracks + 'bass.mid'
-# filename_melody = dir_tracks + 'melody.mid'
-filename_chords = dir_tracks + 'chords.mid'
-# filename_segments = dir_tracks + 'segments.mid'
-# filename_keys = dir_tracks + 'keys.mid'
+filename_to_channel_mapping = {
+    'chords_and_bass.mid': 1,
+    'melody_unrepaired.mid': 2
+}
 
-filename_out = dir_tracks + 'merged.mid'
-
-channel_bass = 1
-channel_chords = 2
-# channel_melody = 3
-# channel_segments = 4
-# channel_keys = 5
-
-# file_melody = MidiFile(filename_melody)
-
-file_bass = MidiFile(filename_bass)
-
-file_chords = MidiFile(filename_chords)
-
-# file_segments = MidiFile(filename_segments)
-
-# file_keys = MidiFile(filename_keys)
-
-# melody = file_melody.tracks[0]
-
-bass = file_bass.tracks[0]
-
-chords = file_chords.tracks[0]
-
-# segments = file_segments.tracks[0]
-
-# keys = file_keys.tracks[0]
+filename_out = dir_tracks + 'merged_test.mid'
 
 
 def copy_track(track, channel, program=None):
@@ -55,40 +27,22 @@ def copy_track(track, channel, program=None):
     return track_new
 
 
-# melody_new = copy_track(track=melody, channel=channel_melody)
-bass_new = copy_track(track=bass, channel=channel_bass)
-chords_new = copy_track(track=chords, channel=channel_chords)
-# segments_new = copy_track(track=segments, channel=channel_segments, program=119)
-# keys_new = copy_track(track=keys, channel=channel_keys)
+file_resolution = MidiFile(dir_tracks + list(filename_to_channel_mapping)[0])
 
+file_out = MidiFile(ticks_per_beat=file_resolution.ticks_per_beat)
 
-file_out = MidiFile(ticks_per_beat=file_chords.ticks_per_beat)
+for filename, channel in filename_to_channel_mapping.items():
 
-# file_out.tracks.append(melody_new)
+    file = MidiFile(dir_tracks + filename)
 
-file_out.tracks.append(bass_new)
+    track = file.tracks[0]
 
-file_out.tracks.append(chords_new)
+    track_new = copy_track(track=track, channel=channel - 1)
 
-# file_out.tracks.append(segments_new)
-
-# file_out.tracks.append(keys_new)
-
-
-# mid = MidiFile()
-# track = MidiTrack()
-# mid.tracks.append(track)
-#
-# track.append(Message('program_change', program=12, time=0))
-# track.append(Message('note_on', note=64, velocity=64, time=32))
-# track.append(Message('note_off', note=64, velocity=127, time=32))
-#
-# mid.save(filename_out)
+    file_out.tracks.append(track_new)
 
 
 file_out.save(filename_out)
-
-# name_program = '/Applications/MuseScore 2.app/Contents/MacOS//mscore'  # '/Applications/MidiYodi 2018.1.app/'
 
 name_program = '/Applications/MidiYodi 2018.1.app/'
 
