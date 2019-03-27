1f221edbc6bf867d0da86dc524345f0b7d67756d
Elliott Evers <everselliott@gmail.com>
2019-03-26 18:15:36 -0600

    experimenting with outlier detection and deleting unused files

--- src/scripts/extract_melody.py
+++ src/scripts/extract_melody.py
@@ -22,6 +22,19 @@ def main(args):
 
     df_melody[df_melody['melody'] < 0] = 0
 
+    df_melody.plot()
+
+    # import numpy as np
+    # from saxpy.hotsax import find_discords_hotsax
+    # import pandas as pd
+    # import matplotlib.pyplot as plt
+
+    # plt.show()
+    # discords = find_discords_hotsax(df_melody.values.reshape(-1, ))
+    #
+    # print(discords)
+
+
     conv_max.to_coll(
         df_melody.rename(columns={'melody': 'signal'}),
         conv_max.file_ts_coll
