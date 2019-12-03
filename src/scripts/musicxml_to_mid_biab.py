9a786c2a2502724ab3d902bfb2dad5e2f558a36a
Elliott Evers <everselliott@gmail.com>
2019-12-03 16:26:35 -0700

    ability to extract bass from BIAB xml

--- src/scripts/musicxml_to_mid_biab.py
+++ src/scripts/musicxml_to_mid_biab.py
@@ -1,14 +1,20 @@
 import sys
 sys.path.insert(0, '/Users/elliottevers/Documents/git-repos.nosync/tk_music_py/src')
 import argparse
-from music21 import converter, harmony, chord, stream, duration
+from music21 import converter, harmony, chord, note, stream, duration
 from utils import utils
+from message import messenger as mes
+from postprocess import music_xml as postp_mxl
 
 
 def main(args):
+    messenger = mes.Messenger()
+
     file_input = utils.parse_arg(args.file_input)
 
     file_output = utils.parse_arg(args.file_output)
+
+    name_part = utils.parse_arg(args.name_part)
 
     score = converter.parse(file_input)
 
@@ -21,14 +27,30 @@ def main(args):
                     chord_symbols = [c for c in m if isinstance(c, harmony.ChordSymbol)]
 
                     for sym in chord_symbols:
-                        chord_new = chord.Chord(
-                            [p.midi for p in sym.pitches],
-                            duration=duration.Duration(4/len(chord_symbols))
-                        )
+                        if name_part == 'chord':
+                            chord_new = chord.Chord(
+                                [p.midi for p in sym.pitches],
+                                duration=duration.Duration(4/len(chord_symbols))
+                            )
 
-                        part_new.append(chord_new)
+                            part_new.append(chord_new)
+                        elif name_part == 'bass':
+                            note_new = note.Note(
+                                [p.midi for p in sym.pitches][0],
+                                duration=duration.Duration(4 / len(chord_symbols))
+                            )
+
+                            part_new.append(note_new)
+
+    if name_part == 'chord':
+        part_new = postp_mxl.force_texture(
+            part_new,
+            num_voices=4
+        )
 
     part_new.write('midi', fp=file_output)
+
+    messenger.message(['done', 'bang'])
 
 
 if __name__ == '__main__':
@@ -37,6 +59,8 @@ if __name__ == '__main__':
     parser.add_argument('--file_input', help='BIAB music xml file')
 
     parser.add_argument('--file_output', help='destination midi file')
+
+    parser.add_argument('--name_part', help='bass or chord')
 
     args = parser.parse_args()
 
